2a5a50de08a30d1b4c517f37668e81dfb0e22932
bharat2006 <bharat38492@gmail.com>
2021-03-09 14:56:56 +0530

    first commit

---
+++ bharat/README.md
@@ -0,0 +1,4 @@
+# DemoAppCode
+# Please check.
+This my website URL..
+https://angularprojectdemo.000webhostapp.com
---
+++ bharat/conn.php
@@ -0,0 +1,13 @@
+<?php
+$conn=mysqli_connect("localhost","root","","book_store");
+error_reporting(1);
+session_start();
+// if($conn)
+ //{
+// 	echo "ok";
+// }
+// else
+// {
+// 	echo "Not Ok";
+// }
+?>
